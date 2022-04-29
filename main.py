f9c4699871b4f38e7acf60f05e7f7f7ce1bafa1c
Wilbert Peter Empleo <55980122+Wilboy-ai@users.noreply.github.com>
2022-04-29 11:14:30 +0200

    First finished draft of dmp code
    Finished dmp class and demo in main

--- main.py
+++ main.py
@@ -3,34 +3,44 @@ import plot_lib as pl
 
 import matplotlib.pyplot as plt
 
-
-dmp_model_x = dmp.dmp(100)
-
-print(dmp_model_x.Ci)
-print(dmp_model_x.hi)
-
-
+#x,y,z = pl.read_T("T.xlsx")
 x,y,z = pl.read_T("Position_testing_2.xlsx")
-#pl.plot_T(x, y, z, 'Position Testing 2')
+#pl.plot_T(x, y, z, 'Demonstration Trajectory')
+
+
+dmp_model_x = dmp.dmp(150)
+dmp_model_y = dmp.dmp(150)
+dmp_model_z = dmp.dmp(150)
 
 
 fdx = dmp_model_x.calc_ft(x)
 dmp_model_x.calc_w(x[0], x[len(x)-1], fdx, len(x))
 
+fdy = dmp_model_y.calc_ft(y)
+dmp_model_y.calc_w(y[0], y[len(y)-1], fdy, len(y))
 
-tx = dmp_model_x._get_T(x[len(x)-1], x[0], len(x))
+fdz = dmp_model_z.calc_ft(z)
+dmp_model_z.calc_w(z[0], z[len(z)-1], fdz, len(z))
 
 
-pl.plot_t(x, 'dmp')
-pl.plot_t(tx, 'dmp')
-#pl.plot_t(ty, 'dmp')
-#pl.plot_t(tz, 'dmp')
+#tx = dmp_model_x._get_T(x[len(x)-1], x[0], len(x))
+#ty = dmp_model_y._get_T(y[len(y)-1], y[0], len(y))
+#tz = dmp_model_z._get_T(z[len(z)-1], z[0], len(z))
+
+#tx = dmp_model_x._get_T(x[len(x)-1]-0.1, x[0], len(x))
+#ty = dmp_model_y._get_T(y[len(y)-1]+0.01, y[0], len(y))
+#tz = dmp_model_z._get_T(z[len(z)-1], z[0], len(z))
+
+tx = dmp_model_x._get_T(x[len(x)-1], x[0] + 0.1, len(x))
+ty = dmp_model_y._get_T(y[len(y)-1], y[0] + 0.1, len(y))
+tz = dmp_model_z._get_T(z[len(z)-1], z[0], len(z))
+
 
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot3D(x, y, z, 'red')
-#ax.plot3D(tx, ty, tz, '--b')
+ax.plot3D(tx, ty, tz, '--b')
 ax.set_title("DMP")
 plt.show()
 
