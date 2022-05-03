aaebd98e0bb1ffb0397c478e5709dfca033bf4a1
Wilbert Peter Empleo <55980122+Wilboy-ai@users.noreply.github.com>
2022-05-03 12:14:50 +0200

    Update
    new trajectories
    force data

--- control.py
+++ control.py
@@ -1,26 +1,36 @@
 import rtde_control
+import time
 
-rtde_c = rtde_control.RTDEControlInterface("192.168.1.100")
+rtde_c = rtde_control.RTDEControlInterface("192.168.1.100", 125)
 
 velocity = 0.1
 acceleration = 0.1
-
 blend = 0.1
 
-point_start = [0.097, -0.380, 0.544, 0.020, -3.172, 0.021, velocity, acceleration, blend]
+#point_start = [0.097, -0.380, 0.544, 0.020, -3.172, 0.021, velocity, acceleration, blend]
+#path = [point_start]
+#point = [0.097, -0.380, 0.544, 0.020, -3.172, 0.021, velocity, acceleration, blend]
 
-path = [point_start]
-
-
-point = [0.097, -0.380, 0.544, 0.020, -3.172, 0.021, velocity, acceleration, blend]
-
-
-
-print(len(path))
-
-
-
+#print(len(path))
 
 # Send a linear path with blending in between - (currently uses separate script)
-rtde_c.moveL(path)
+
+dt = 1.0 / 1
+lookahead_time = 0.1
+gain = 100
+
+point_start = (0.097, -0.380, 0.544, 0.020, -3.172, 0.021)
+
+for i in range(1, 100):
+    start = time.time()
+    rtde_c.servoL(point_start, velocity, acceleration, dt, lookahead_time, gain)
+    end = time.time()
+    duration = end - start
+    if duration < dt:
+        time.sleep(dt - duration)
+
+rtde_c.servoStop()
 rtde_c.stopScript()
+
+
+
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import plot_lib as pl
 import time
 import numpy as np
 
+import rtde_control
+import rtde_receive
+
 import matplotlib.pyplot as plt
 
 
@@ -37,11 +40,10 @@ def run_robot(T):
     time.sleep(0.3)
 
     try:
-        rob.movels(T, acc=a, vel=v, radius=r, wait=True, threshold=0.1)
-
-        time.sleep(1.5)
-        print("Returning home")
-        rob.movel((0.097, -0.380, 0.544, 0.020, -3.172, 0.021), a, v, wait=True)
+        rob.movels(T, acc=a, vel=v, radius=r, wait=True, threshold=0.01)
+        #time.sleep(1.5)
+        #print("Returning home")
+        #rob.movel((0.097, -0.380, 0.544, 0.020, -3.172, 0.021), a, v, wait=True)
 
         rob.close()
         print("Trajectory Finished!")
@@ -49,6 +51,44 @@ def run_robot(T):
         rob.close()
         print("Error! Stopped the robot!")
 
+
+def realTime_run(x, y, z):
+
+    rtde_c = rtde_control.RTDEControlInterface("192.168.1.100")
+    rtde_r = rtde_receive.RTDEReceiveInterface("192.168.1.100")
+
+    x.reverse()
+    y.reverse()
+    z.reverse()
+
+    velocity = 0.1
+    acceleration = 0.1
+    dt = 1.0 / 50
+    lookahead_time = 0.1
+    gain = 300
+    tcp_place = [x[0], y[0], z[0], 0.020, -3.172, 0.021]
+
+    # move to initial tcp pos:
+    # rtde_c.moveJ(joint_q)
+    rtde_c.moveL(tcp_place, velocity, acceleration)
+
+    # execute 500Hz control loop for 2 seconds, each cycle is 2ms
+
+    for i in range(1, len(x)):
+        start = time.time()
+        rtde_c.servoL(tcp_place, velocity, acceleration, dt, lookahead_time, gain)
+        tcp_place[0] = x[i]
+        tcp_place[1] = y[i]
+        tcp_place[2] = z[i]
+        end = time.time()
+        duration = end - start
+        if duration < dt:
+            time.sleep(dt - duration)
+
+    print(f"x: {tcp_place[0]}, y: {tcp_place[1]}, z: {tcp_place[2]}")
+
+    rtde_c.servoStop()
+    rtde_c.stopScript()
 
 
 rob = urx.Robot('192.168.1.100')
@@ -63,7 +103,7 @@ print(target_pos[1])
 print(target_pos[2])
 
 #x,y,z = pl.read_T("T.xlsx")
-x,y,z = pl.read_T("trajectory_02.xlsx", 0.001)
+x,y,z = pl.read_T("t01.xlsx", 0.001)
 #pl.plot_T(x, y, z, 'Demonstration Trajectory')
 
 N = 100
@@ -126,13 +166,15 @@ ax.set_title("DMP Trajectory")
 plt.show()
 
 
+#T1 = create_T(tx,ty,tz, True)
+#run_robot(T1)
+
+realTime_run(tx, ty, tz)
+
+#T = create_T(tx0,ty0,tz0, True)
+#run_robot(T)
 
 
-T1 = create_T(tx,ty,tz, True)
-run_robot(T1)
-
-T = create_T(tx0,ty0,tz0, True)
-run_robot(T)
 
 
 
@@ -145,7 +187,7 @@ run_robot(T)
 '''
 import rtde_control
 
-rtde_c = rtde_control.RTDEControlInterface("192.168.1.100")
+rtde_c = rtde_control.RTDEControlInterface("192.168.1.100", 125)
 
 velocity = 0.2
 acceleration = 0.2
